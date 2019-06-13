594b507070f18240e3d4f76d1c9fd983f83790a8
Brian Medendorp <brian@medendorponline.com>
2019-06-13 13:09:30 -0700

    Add recycler view click listener (gesture detector)

---
+++ app/src/main/java/com/tythis/flickrbrowser/RecyclerItemClickListener.java
@@ -0,0 +1,33 @@
+package com.tythis.flickrbrowser;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+class RecyclerItemClickListener extends RecyclerView.SimpleOnItemTouchListener {
+    private static final String TAG = "RecyclerItemClickListen";
+
+    interface OnRecyclerClickListener {
+        void onItemClick(View view,int position);
+        void onItemLongClick(View view, int position);
+    }
+
+    private final OnRecyclerClickListener mListener;
+    private final GestureDetectorCompat mGestureDetector;
+
+    public RecyclerItemClickListener(Context context, final RecyclerView recyclerView, OnRecyclerClickListener listener) {
+        mListener = listener;
+        mGestureDetector = null;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+        Log.d(TAG, "onInterceptTouchEvent: starts");
+        return super.onInterceptTouchEvent(rv, e);
+    }
+}
